561f68a3e61a28d38f7eaf5ea4ec61754cbc821f
WhaleToaster <sollkre@gmail.com>
2025-06-11 20:34:05 +0200

    on the web baby

--- corner-preload.js
+++
@@ -1,5 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('api', {
-  cornerClicked: () => ipcRenderer.send('corner-clicked')
-});
--- preload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('api', {
-  launch: (data) => ipcRenderer.send('launch', data),
-});
-
